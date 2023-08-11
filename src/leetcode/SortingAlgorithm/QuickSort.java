5aa3dfe0924e29096ed4bb29fd0173c1d5d119b7
qingtong <jp.wu@tuya.com>
2023-08-11 18:39:29 +0800

    update

--- src/leetcode/SortingAlgorithm/QuickSort.java
+++ src/leetcode/SortingAlgorithm/QuickSort.java
@@ -6,38 +6,38 @@ package leetcode.SortingAlgorithm;
  **/
 public class QuickSort {
 
-    /**
-     * 获取哨兵
-     *
-     * @param nums
-     * @param left
-     * @param right
-     */
+    public static void sort(int[] nums, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int part = partition(nums, left, right);
+        sort(nums, left, part - 1);
+        sort(nums, part + 1, right);
+    }
+
+
+
     public static int partition(int[] nums, int left, int right) {
         int i = left;
         int j = right;
         while (i < j) {
-            while (i < j && nums[j] >= nums[left]) j--;
-            while (i < j && nums[i] <= nums[left]) i++;
+            while (i < j && nums[left] <= nums[j]) {
+                j--;
+            }
+            while (i < j && nums[left] >= nums[i] ) {
+                i++;
+            }
             swap(nums, i, j);
         }
         swap(nums, left, i);
         return i;
     }
 
-    public static void swap(int[] nums, int x, int y) {
-        int tmp = nums[x];
-        nums[x] = nums[y];
-        nums[y] = tmp;
-    }
-
-    public static void sort(int[] nums, int left, int right) {
-        if (left >= right) {
-            return;
-        }
-        int part = partition(nums, left, right);
-        sort(nums, left, part - 1);
-        sort(nums, part + 1, right);
+    public static void swap(int[] nums, int left, int right) {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
     }
 
     public static void main(String[] args) {
